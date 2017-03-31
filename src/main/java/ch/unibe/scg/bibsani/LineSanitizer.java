58a6e8ac8ab49df983aace0e8d9f00b93310dbf7
Lukas Imstepf <non@sense.ch>
2017-03-31 15:49:31 +0200

    Initial commit

---
+++ src/main/java/ch/unibe/scg/bibsani/LineSanitizer.java
@@ -0,0 +1,88 @@
+package ch.unibe.scg.bibsani;
+
+import java.text.Normalizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Line sanitizers.
+ */
+public enum LineSanitizer {
+
+	/**
+	 * Copy. As if I give a fuck...
+	 */
+	COPY() {
+				@Override
+				public String sanitize(String line) {
+					return line;
+				}
+			},
+	/**
+	 * Remove funny UTF-8 accents from BibTeX key/entry start lines.
+	 */
+	REMOVE_ACCENTS_IN_KEYS() {
+				@Override
+				public String sanitize(String line) {
+					if (isEntryStart(line)) {
+						return removeAccents(line);
+					}
+					return line;
+				}
+			},
+	/**
+	 * Remove funny UTF-8 accents from BibTeX key/entry start lines, then remove
+	 * all illegal chars.
+	 */
+	REMOVE_ACCENTS_THEN_INVALID_CHARS_IN_KEYS() {
+				@Override
+				public String sanitize(String line) {
+					if (isEntryStart(line)) {
+						final String noaccents = removeAccents(line);
+						return removeInvalidChars(noaccents);
+					}
+					return line;
+				}
+			},
+	/**
+	 * Remove all illegal chars.
+	 */
+	REMOVE_INVALID_CHARS_IN_KEYS() {
+				@Override
+				public String sanitize(String line) {
+					if (isEntryStart(line)) {
+						return removeInvalidChars(line);
+					}
+					return line;
+				}
+			};
+
+	private static final String WHITELIST_CHARS_PATTERN = "[^\\sa-zA-Z0-9.,:;\\?!`'\\(\\)\\{\\}\\[\\]/@_\\+=-]";
+	private static final String ACCENT_PATTERN = "\\p{InCombiningDiacriticalMarks}+";
+	private static final Pattern ENTRY_START_LINE_PATTERN = Pattern.compile(
+			"^\\s*@([a-zA-Z]+)\\{"
+	);
+
+	private static boolean isEntryStart(String line) {
+		final Matcher m = ENTRY_START_LINE_PATTERN.matcher(line);
+		return m.find();
+	}
+
+	private static String removeAccents(String line) {
+		line = line.replaceAll("ß", "ss");
+		return Normalizer.normalize(line, Normalizer.Form.NFD).replaceAll(ACCENT_PATTERN, "");
+	}
+
+	private static String removeInvalidChars(String line) {
+		return line.replaceAll(WHITELIST_CHARS_PATTERN, "");
+	}
+
+	/**
+	 * Sanitizes the line.
+	 *
+	 * @param line the dirty line.
+	 * @return the sanitized line.
+	 */
+	abstract public String sanitize(String line);
+
+}
---
+++ src/main/java/ch/unibe/scg/bibsani/Main.java
@@ -0,0 +1,144 @@
+package ch.unibe.scg.bibsani;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * PDF database Scrap(er).
+ */
+public class Main {
+
+	/**
+	 * Main method.
+	 *
+	 * @param args the command line arguments.
+	 */
+	public static void main(String[] args) {
+
+		// parse command line arguments
+		final CommandLineArguments cla = new CommandLineArguments(Main.class, args);
+		final CommandLineArguments.Argument fileArg = cla.add(
+				"A bibtex (BIB) file to process.",
+				"<file>",
+				"f", "file"
+		);
+		final CommandLineArguments.Argument dirArg = cla.add(
+				"The output directory where to put the sanitized BibTeX file. (OPTIONAL)",
+				"<file>",
+				"d", "directory"
+		);
+		final CommandLineArguments.Argument usageArg = cla.add(
+				"Print the usage of this program.",
+				"",
+				"u", "usage"
+		);
+
+		if (args.length == 0 || usageArg.isSet() || !CommandLineArguments.areAllSet(fileArg)) {
+			cla.printUsage();
+			kthxbai();
+		}
+
+		final File inputFile = new File(fileArg.getString());
+		if (!inputFile.exists()) {
+			printError("ERROR: input file does not exists: " + inputFile);
+			kthxbai();
+		}
+		System.out.println("input file: " + inputFile);
+
+		File outputDirectory = null;
+		if (!dirArg.isEmpty()) {
+			outputDirectory = new File(dirArg.getString());
+			if (!outputDirectory.exists()) {
+				System.out.println("creating output directory: " + outputDirectory);
+				outputDirectory.mkdirs();
+			}
+		}
+		// just put output file where the input file is if no directory is specified
+		if (outputDirectory == null) {
+			outputDirectory = inputFile.getParentFile();
+		}
+		final File outputFile = new File(
+				outputDirectory,
+				getOutputFilename(inputFile)
+		);
+		System.out.println("output file: " + outputFile);
+
+		final LineSanitizer sanitizer = LineSanitizer.REMOVE_ACCENTS_THEN_INVALID_CHARS_IN_KEYS;
+		int numLine = 0;
+		int numSanitizedLines = 0;
+
+		try (BufferedReader reader = newReader(inputFile)) {
+			try (BufferedWriter writer = newWriter(outputFile)) {
+				String line;
+				while ((line = reader.readLine()) != null) {
+					numLine++;
+					final String sanitizedLine = sanitizer.sanitize(line);
+					if (!line.equals(sanitizedLine)) {
+						numSanitizedLines++;
+						System.out.println(String.format(
+								"line %7d: %s",
+								numLine, line
+						));
+						System.out.println(String.format(
+								" sanitized to: %s",
+								sanitizedLine
+						));
+					}
+					writer.write(sanitizedLine);
+					writer.newLine();
+				}
+			} catch (IOException ex) {
+				printError(ex, "ERROR: failed to write to output file: " + outputFile);
+			}
+		} catch (IOException ex) {
+			printError(ex, "ERROR: failed to read input file: " + inputFile);
+		}
+
+		kthxbai();
+	}
+
+	public static void kthxbai() {
+		System.out.println("\nkthxbai.");
+		System.exit(0);
+	}
+
+	public static void printError(String... messages) {
+		printError(null, messages);
+	}
+
+	public static void printError(Exception ex, String... messages) {
+		for (String msg : messages) {
+			System.err.println(msg);
+		}
+		if (ex != null) {
+			ex.printStackTrace(System.err);
+		}
+	}
+
+	public static String getOutputFilename(File file) {
+		final String filename = file.getName();
+		final int n = filename.lastIndexOf('.');
+		return filename.substring(0, n) + "-bibsani.bib";
+	}
+
+	public static BufferedReader newReader(File file) throws UnsupportedEncodingException, FileNotFoundException {
+		return new BufferedReader(new InputStreamReader(
+				new FileInputStream(file), "UTF-8"
+		));
+	}
+
+	public static BufferedWriter newWriter(File file) throws UnsupportedEncodingException, FileNotFoundException {
+		return new BufferedWriter(new OutputStreamWriter(
+				new FileOutputStream(file), "UTF-8"
+		));
+	}
+
+}
